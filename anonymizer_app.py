c214bb717e318dffd692123a9135f4a3e68ac7e1
icuriouswiz <73981868+cloneartist@users.noreply.github.com>
2023-04-10 17:36:00 +0530

    add diff privacy

--- anonymizer_app.py
+++ anonymizer_app.py
@@ -6,6 +6,8 @@ from presidio_analyzer import AnalyzerEngine, Pattern, PatternRecognizer
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut, GeocoderServiceError
 from datetime import datetime
+import numpy as np
+from diffprivlib.mechanisms import Laplace
 
 st.title("Data Anonymizer")
 
@@ -19,12 +21,10 @@ if uploaded_file is not None:
     st.write(df)
 
     # Define the regex pattern in a Presidio `Pattern` object:
-    numbers_pattern = Pattern(name="numbers_pattern",
-                              regex="[A-Z]{5}[0-9]{4}[A-Z]{1}", score=0.5)
+    numbers_pattern = Pattern(name="numbers_pattern", regex="[A-Z]{5}[0-9]{4}[A-Z]{1}", score=0.5)
 
     # Define the recognizer with one or more patterns
-    number_recognizer = PatternRecognizer(
-        supported_entity="PAN_NUMBER", patterns=[numbers_pattern])
+    number_recognizer = PatternRecognizer(supported_entity="PAN_NUMBER", patterns=[numbers_pattern])
 
     # Analyzer output
     analyzer = AnalyzerEngine()
@@ -79,37 +79,41 @@ if uploaded_file is not None:
 
     # k-anonymity
     # Identify the sensitive attributes
-    sensitive_attributes = ['User Id', 'Name',
-                            'Twitter Username', 'User Bio', 'Profile URL']
+    sensitive_attributes = ['User Id', 'Name', 'Twitter Username', 'User Bio', 'Profile URL']
 
     # Determine the K value
     K = 5
 
     # Identify the quasi-identifiers
-    quasi_identifiers = ['Tweet Location', 'User Followers', 'User Following',
-                         'User Account Creation Date', 'Tweet Posted Time (UTC)']
+    quasi_identifiers = ['Tweet Location', 'User Followers', 'User Following', 'User Account Creation Date', 'Tweet Posted Time (UTC)']
 
     # Group the individuals based on the quasi-identifiers
     groups = df.groupby(quasi_identifiers)
 
     # Function to generalize Tweet Location to broader geographic regions
     def generalize_location(location):
-        geolocator = Nominatim(user_agent="my-app")
-        try:
-            location = geolocator.geocode(location)
-            if location is not None:
-                lat = location.raw.get('lat')
-                lon = location.raw.get('lon')
-                loc = lat + ',' + lon
-                country = geolocator.reverse(loc)
-                return country.raw.get('address').get('country')
-            return "Unknown"
-        except (GeocoderTimedOut, GeocoderServiceError):
-            return "Unknown"
+            geolocator = Nominatim(user_agent="my-app")
+            try:
+                location = geolocator.geocode(location)
+                if location is not None:
+                    lat= location.raw.get('lat')
+                    lon= location.raw.get('lon')
+                    loc = lat + ',' + lon
+                    country = geolocator.reverse(loc)
+                    return country.raw.get('address').get('country')
+                return "Unknown"
+            except (GeocoderTimedOut, GeocoderServiceError):
+                return "Unknown"
+
 
     # Function to generalize User Followers and User Following to ranges
-
     def generalize_follower_following(count):
+        sensitivity=1
+        epsilon=1
+        beta=sensitivity/epsilon
+        noise = Laplace(epsilon=epsilon, sensitivity=sensitivity).randomise(value=1)
+        return count + noise.astype(int)
+
         if count < 100:
             return '0-99'
         elif count >= 100 and count < 500:
@@ -136,33 +140,27 @@ if uploaded_file is not None:
         for col in quasi_identifiers:
             if col == 'Tweet Location':
                 # Generalize Tweet Location to broader geographic regions
-                generalized_group[col] = group[col].apply(
-                    lambda x: generalize_location(x))
+                generalized_group[col] = group[col].apply(lambda x: generalize_location(x))
             elif col == 'User Followers' or col == 'User Following':
                 # Generalize User Followers and User Following to ranges
-                generalized_group[col] = group[col].apply(
-                    lambda x: generalize_follower_following(x))
+                generalized_group[col] = group[col].apply(lambda x: generalize_follower_following(x))
             elif col == 'User Account Creation Date':
                 # Generalize User Account Creation Date to month and year
-                generalized_group[col] = group[col].apply(
-                    lambda x: generalize_account_creation_date(x))
+                generalized_group[col] = group[col].apply(lambda x: generalize_account_creation_date(x))
             elif col == 'Tweet Posted Time (UTC)':
                 # Generalize User Account Creation Date to month and year
-                generalized_group[col] = group[col].apply(
-                    lambda x: generalize_account_creation_date(x))
+                generalized_group[col] = group[col].apply(lambda x: generalize_account_creation_date(x))
         generalized_data.append(generalized_group)
 
     # Create a new DataFrame with the anonymized quasi-identifiers and the non-sensitive attribute Tweet Content
-    anonymized_data = pd.concat(generalized_data)[
-        ['Tweet Location', 'User Followers', 'User Following', 'User Account Creation Date', 'Tweet Content', 'Tweet Posted Time (UTC)']]
+    anonymized_data = pd.concat(generalized_data)[['Tweet Location', 'User Followers', 'User Following', 'User Account Creation Date', 'Tweet Content', 'Tweet Posted Time (UTC)']]
 
     # Verify that each group satisfies the K-Anonymity condition and merge groups if necessary
     k_anonymized_data = []
     for name, group in anonymized_data.groupby(quasi_identifiers):
         if len(group) < K:
             # Merge the group with the nearest group that satisfies the K-Anonymity condition
-            merged_group = pd.concat([group, anonymized_data.loc[anonymized_data.groupby(
-                quasi_identifiers).groups[name]].iloc[0:K-len(group)]])
+            merged_group = pd.concat([group, anonymized_data.loc[anonymized_data.groupby(quasi_identifiers).groups[name]].iloc[0:K-len(group)]])
             k_anonymized_data.append(merged_group)
         else:
             k_anonymized_data.append(group)
